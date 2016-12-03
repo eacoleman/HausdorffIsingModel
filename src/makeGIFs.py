d6616f35da05622965ae2210b874a70a971ddaab
Evan Coleman <ecoleman@fnal.gov>
2016-12-02 21:20:05 -0600

    Implement 3D rotation GIF creation

--- src/makeGIFs.py
+++ src/makeGIFs.py
@@ -8,7 +8,7 @@ import pickle
 import numpy
 
 from PIL import Image, ImageSequence
-from images2gif import writeGif
+from interface/images2gif import writeGif
 
 ROOT.gSystem.Load("libHiggsAnalysisCombinedLimit")
 #ROOT.gROOT.SetBatch()
